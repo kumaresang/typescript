de78d9fd980426d3f06a388cd7528ce0ff8cd371
Kumaresan Govindan <kumaresan.govindan@outlook.com>
2019-06-05 14:00:06 +0530

    write logic to wirte log.

--- main.ts
+++ main.ts
@@ -3,3 +3,4 @@ import {Manager,TeamLead,Person} from './class/index'
 
 var manager:Manager = new Manager("Kumarsan", "Govindan");
 console.log(manager.getFullName());
+console.log(manager.getSalary());
